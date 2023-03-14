9a244ad75db06a58f1211019bbbed2f9ac0987f1
DavidFerH <davidferreroherrera@gmail.com>
2023-03-14 20:17:19 +0100

    Creacion del proyecto de Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layout/template')
+
+@section('pageTitle', 'Bienvenidos a la revista de la UI1')
+    
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <h1>¡Bienvenidos a la revista de la UI1!</h1>
+        </div>
+    </div>
+@endsection
