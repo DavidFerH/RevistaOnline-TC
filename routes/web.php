2f09a1ae2ffb6b1e8840a94ad49e72e67004d14d
DavidFerH <davidferreroherrera@gmail.com>
2023-03-15 21:32:38 +0100

    Avances

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticulosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [ArticulosController::class, 'index']) -> name('personas.index');
+Route::get('/create', [ArticulosController::class, 'create']) -> name('personas.create');
