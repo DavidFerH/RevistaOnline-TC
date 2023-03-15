18425ec945f905fea5ff526a37613faf9dff2226
DavidFerH <davidferreroherrera@gmail.com>
2023-03-15 21:27:20 +0100

    Avances

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,7 @@
 @section('pageTitle', 'Bienvenidos a la revista de la UI1')
     
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <h1>¡Bienvenidos a la revista de la UI1!</h1>
-        </div>
+    <div class="mx-auto mt-5">
+        <h3>Bienvenido a la revista online de la Univesidad Isabel I</h3>
     </div>
 @endsection
