8ccd8ad7ca970e3d2cc2bcf5ba70728d31043cc2
DavidFerH <davidferreroherrera@gmail.com>
2023-03-19 00:27:53 +0100

    solucionando conlfictos

--- app/Models/Articulos.php
+++ app/Models/Articulos.php
@@ -7,18 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class articulos extends Model {
     use HasFactory;
-<<<<<<< HEAD
-    // Para no meter updated_at and created_at
-    public $timestamps = false;
-=======
     public $timestamps = false; //para no meter updated_at and created_at
 
 
-    public function autor()
-    {
+    public function autor() {
         return $this->belongsToMany(Autor::class, 'autor_articulos', 'DNI', 'DNI');
     }
-
-
->>>>>>> 77ac5faad1689f34f217276a9bf308d4a88d9b3a
 }
--- app/Models/autor.php
+++ app/Models/autor.php
@@ -8,17 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class autor extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
-    // Si no eloquent mete la s al final, autors.
-    protected $table = 'autor';
-=======
     protected $table = 'autor'; //sino eloquent mete la s al final, autors.
 
-    public function articulos()
-    {
+    public function articulos() {
         return $this->belongsToMany(Articulos::class, 'autor_articulos', 'COD_ART', 'COD_ART');
     }
-
-
->>>>>>> 77ac5faad1689f34f217276a9bf308d4a88d9b3a
 }
